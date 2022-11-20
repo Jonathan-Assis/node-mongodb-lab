b21a0f64243df059826928ea96c2530e105f4bdd
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-11-20 18:37:29 -0300

    Create a new database and using the insertOne() method
    insertOne() to insert a single document into a collection, in this case the database bank and the collection accounts do not exist, if the insertion occurs, it will be automatically created

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
+// Require MongoDB language driver
 const { MongoClient } = require("mongodb")
 require("dotenv").config();
 
 // Use .env to specify the Atlas MongoDB uri connection
-const mongoConnection = process.env.ATLAS_MONGODB_URI
-// Create the MongoClient instance
-const client = new MongoClient(mongoConnection)
+const uri = process.env.ATLAS_MONGODB_URI
+const client = new MongoClient(uri)
+
+// Bank account example
+const dbname= "bank"
+const collection_name = "accounts"
+
+// Select the database and collection instance
+const accountsCollection = client.db(dbname).collection(collection_name)
 
 // Establishes a connection to the database using the MongoClient instance
 const connectToDatabase = async () => {
   try {
     await client.connect()
-    console.log("Connected to MongoDB Atlas!")
-    // list out all the databases in the cluster
-    const dbs = await client.db().admin().listDatabases()
-    console.table(dbs.databases)
+    console.log(`Connected to the ${dbname} database, \nFull connection string: ${uri}`)
   } catch (error) {
-    console.error(error)
+    console.error(`Error connecting to the database: ${error}`)
+  }
+}
+
+// Simple mock
+const sampleAccount = {
+  account_holder: "Robervaldo Lopes",
+  account_id: "MDB829001337",
+  account_type: "checking",
+  balance: 50352434,
+  last_updated: new Date(),
+}
+
+const main = async () => {
+  try {
+    await connectToDatabase()
+    // 'Insert one' document by using the sampleAccount mock
+    let result = await accountsCollection.insertOne(sampleAccount)
+    console.log(`Inserted document: ${result.insertedId}`)
+  } catch (error) {
+    console.error(`Error inserting document: ${error}`)
   } finally {
+    // close the client connection
     await client.close()
   }
 }
 
-// Run the `connectToDatabase` function, catch any errors and finally close the connection when the connectToDatabase function is done
-connectToDatabase()
-  .catch((err) => console.log(err))
-  .finally(() => client.close())
+main()
