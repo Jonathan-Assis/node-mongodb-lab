e3fc2481468483775875610fd33783617056ac33
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-11-21 01:10:13 -0300

    Using aggregate() method
    The aggregate method takes an array of stages as an argument, which is stored in a variable. The aggregate method returns a cursor that we can iterate over to get the results.

--- app.js
+++ app.js
@@ -6,90 +6,33 @@ require("dotenv").config();
 const uri = process.env.ATLAS_MONGODB_URI
 const client = new MongoClient(uri)
 
-// Step 1: Create variables used in the transaction.  
-// Collections
-const accounts = client.db("bank").collection("accounts")
-const transfers = client.db("bank").collection("transfers")
+const dbname = "bank";
+const collection_name = "accounts";
+const accountsCollection = client.db(dbname).collection(collection_name);
 
-// Account information
-let account_id_sender = "MDB829001337"
-let account_id_receiver = "MDB829000001"
-let transaction_amount = 100
-
-// Step 2:  Start a new session. 
-const session = client.startSession()
-
-// use withTransaction to start a transaction, execute the callback, and commit the transaction
-// The callback for withTransaction must be async/await
-// Note: Each individual operation must be awaited and have the session passed in as an argument
-// Establishes a connection to the database using the MongoClient instance
-
+const pipeline = [
+  // Stage 1: match the accounts with a balance greater than $1,000
+  { $match: { balance: { $lt: 1000 } } },
+  // Stage 2: Calculate average balance and total balance
+  {
+    $group: {
+      _id: "$account_type",
+      total_balance: { $sum: "$balance" },
+      avg_balance: { $avg: "$balance" },
+    },
+  },
+]
 const main = async () => {
   try {
-    // Step 3: Begin a transaction with the WithTransaction() method on the session. 
-    const transactionResults = await session.withTransaction(async () => {
-      // Step 4: Update the balance field of the sender’s account by decrementing the transaction_amount from the balance field. 
-      const senderUpdate = await accounts.updateOne(
-        { account_id: account_id_sender },
-        { $inc: { balance: -transaction_amount } },
-        { session }
-      )
-      console.log(
-        `${senderUpdate.matchedCount} document(s) matched the filter, updated ${senderUpdate.modifiedCount} document(s) for the sender account`
-      )
-
-      // Step 5: Update the balance field of the receiver’s account by incrementing the transaction_amount to the balance field. 
-      const receiverUpdate = await accounts.updateOne(
-        { account_id: account_id_receiver },
-        { $inc: { balance: transaction_amount } },
-        { session }
-      )
-      console.log(
-        `${receiverUpdate.matchedCount} document(s) matched the filter, updated ${receiverUpdate.modifiedCount} document(s) for the receiver account`
-      )
-
-      // Step 6: Create a transfer document and insert it into the transfers collection. 
-      const transfer = {
-        transfer_id: "TR21872187",
-        amount: 100,
-        from_account: account_id_sender,
-        to_account: account_id_receiver,
-      }      
-
-      const insertTransferResults = await transfers.insertOne(transfer, { session })
-      console.log(`Sucessfully inserted ${insertTransferResults.insertedId} into the transfers collection`)
-      
-      // Step 7: Update the transfers_complete array of the sender’s account by adding the transfer_id to the array. 
-      const updateSenderTransferResults = await accounts.updateOne(
-        { account_id: account_id_sender },
-        { $push: { transfers_complete: transfer.transfer_id } },
-        { session }
-      )
-      console.log(`${updateSenderTransferResults.matchedCount} document(s) matched in the transfers collection, updated ${updateSenderTransferResults.modifiedCount}`)
-
-      // Step 8: Update the transfers_complete array of the receiver’s account by adding the transfer_id to the array. 
-      const updateReceiverTransferResults = await accounts.updateOne(
-        { account_id: account_id_receiver },
-        { $push: { transfers_complete: transfer.transfer_id } },
-        { session }
-      )
-      console.log(`${updateReceiverTransferResults.matchedCount} document(s) matched in the transfers collection, updated ${updateReceiverTransferResults.modifiedCount}`)
-    })
-
-    console.log("Committing transaction...")
-    // Step 9: Log a message regarding the success or failure of the transaction. 
-    // If the callback for withTransaction returns successfully without throwing an error, the transaction will be committed 
-    if (transactionResults){
-      console.log("The reservation was sucessfully created.")
-    } else {
-      console.log("The reservation was intentionally aborted.")
+    await client.connect()
+    console.log(`Connected to the database 🌍. \nFull connection string: ${uri}`)
+    let result = await accountsCollection.aggregate(pipeline)
+    for await (const doc of result) {
+      console.log(doc)
     }
-  } catch (error) {
-    // Step 10: Catch any errors and close the session.
-    console.error(`Transaction aborted: ${error}`)
-    process.exit(1)
+  } catch (err) {
+    console.error(`Error connecting to the database: ${err}`)
   } finally {
-    await session.endSession()
     await client.close()
   }
 }
